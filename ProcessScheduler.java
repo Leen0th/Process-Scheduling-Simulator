f47770ee7060ab16a77e5f6c1637fe30f68357e5
Leen Thamer <89545353+Leen0th@users.noreply.github.com>
2025-02-07 12:16:56 +0300

    add error handling

--- ProcessScheduler.java
+++ ProcessScheduler.java
@@ -1,29 +1,22 @@
 import java.util.*;
 
 class ProcessScheduler {
-    
+
     public static List<Process> getProcesses() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of processes: ");
-        int numProcesses = sc.nextInt();
+        int numProcesses = getValidInteger(sc, "Enter the number of processes: ", 1);
 
         List<Process> processes = new ArrayList<>();
         for (int i = 0; i < numProcesses; i++) {
-            System.out.println("Enter arrival time for P" + (i + 1) + ":");
-            int arrivalTime = sc.nextInt();
-
-            System.out.println("Enter burst time for P" + (i + 1) + ":");
-            int burstTime = sc.nextInt();
-
+            int arrivalTime = getValidInteger(sc, "Enter arrival time for P" + (i + 1) + ": ", 0);
+            int burstTime = getValidInteger(sc, "Enter burst time for P" + (i + 1) + ": ", 1);
             processes.add(new Process(i + 1, arrivalTime, burstTime));
         }
 
-        sc.close();
         return processes;
     }
 
     public static void displayProcesses(List<Process> processes) {
-        
         System.out.print("Number of processes= " + processes.size() + " (");
         for (int i = 0; i < processes.size(); i++) {
             System.out.print("P" + processes.get(i).getId());
@@ -46,5 +39,23 @@ class ProcessScheduler {
         // Placeholder for scheduling algorithm
         System.out.println("Scheduling Algorithm: Shortest remaining time first");
         System.out.println("Context Switch: 1 ms");
+    }
+
+    private static int getValidInteger(Scanner sc, String prompt, int min) {
+        int value;
+        while (true) {
+            System.out.print(prompt);
+            if (sc.hasNextInt()) {
+                value = sc.nextInt();
+                if (value >= min) {
+                    return value;
+                } else {
+                    System.out.println("Invalid input. Please enter a number greater than or equal to " + min + ".");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                sc.next(); // Clear invalid input
+            }
+        }
     }
 }
