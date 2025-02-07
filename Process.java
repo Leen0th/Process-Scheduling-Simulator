f768ad674d5b91d09d0a7fd4c058dc2ce2c9db5d
Leen Thamer <89545353+Leen0th@users.noreply.github.com>
2025-02-07 11:43:42 +0300

    add att & methodes to help with scheduling calculations and performance metrics

--- Process.java
+++ Process.java
@@ -1,26 +1,65 @@
+import java.util.*;
+
 class Process {
     private int id;
     private int arrivalTime;
     private int burstTime;
-    
+    private int remainingTime; // to help with scheduling calculations and performance metrics
+    private int completionTime; // to help with scheduling calculations and performance metrics
+    private int waitingTime; // to help with scheduling calculations and performance metrics
+    private int turnaroundTime; // to help with scheduling calculations and performance metrics
+
     public Process(int id, int arrivalTime, int burstTime) {
         this.id = id;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
+        this.remainingTime = burstTime;
     }
-    
+
     public int getId() {
         return id;
     }
-    
+
     public int getArrivalTime() {
         return arrivalTime;
     }
-    
+
     public int getBurstTime() {
         return burstTime;
     }
-    
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
     @Override
     public String toString() {
         return "P" + id + ": Arrival time = " + arrivalTime + ", Burst time = " + burstTime + " ms";
